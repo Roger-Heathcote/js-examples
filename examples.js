3ec49e04f5492f68fab194066fd5b9bae423c774
Roger Heathcote <git@technicalbloke.com>
2020-03-26 09:26:19 +0000

    Generalized the examples a bit more.

--- examples.js
+++ examples.js
@@ -11,7 +11,7 @@ let decodeJSON = res => res.json;
 
 function fetchSomething(searchString, callback, errorHandler){
     console.log("Searching API for ", searchString);
-    initOptions = { headers : {'Authtoken':'3X5BkCsjR5Rd5TfElUk7Oj'} }
+    initOptions = { headers : {'some-token':'3X5BkCsjR5Rd5TfElUk7Oj'} }
     fetch(`https://example.com/search?$${searchString}`, initOptions)
     .then(dieIfBadResponse)  // the object fetch returns should have a property .ok
     .then(decodeJSON)        // we need to run the .json() method on the raw object
@@ -70,7 +70,7 @@ const types = {
 
 function publicHandler(request, response) {
   let pathBits = request.url.split("/");
-  let extension = pathBits[pathBits.length-1].split(".")[1]; //TODO robustness
+  let extension = pathBits[pathBits.length-1].split(".")[1] || "";
   let newPath = path.join(__dirname, "..", ...pathBits);
   fs.readFile(newPath, (error, file) => {
     if (error) {
@@ -90,24 +90,14 @@ module.exports = publicHandler;
 // Template cloning and populating
 // Template cloning and populating
 
-function addTask(priority, text, checkBox){
-    
-    let template = document.getElementById('cardTemplate');
-    let newTask = template.content.cloneNode(true);
-    let thePriority = newTask.querySelector(".to-do-list__card__priority");
-    thePriority.classList.add(priority);
-    let theText = newTask.querySelector(".to-do-list__card__text");
-    theText.textContent = text;
-    let theStatus = newTask.querySelector(".to-do-list__card__checkbox");
-    theStatus.checked = checkBox;
-    
-    // If user clicks on the text it should re-open in the edit window
-    theText.addEventListener("click", editHandler);
-
-    // If user checks the checkbox the task is moved to the done list
-    theStatus.addEventListener("click", checkboxHandler);
-
-    toDoList.appendChild(newTask);
+function addItem(text, checkBox){    
+    let template = document.getElementById('template');
+    let newItem = template.content.cloneNode(true);
+    newItem.querySelector(".item__text") = text;
+    let itemCheckBox = newItem.querySelector(".item__checkbox");
+    itemCheckBox.checked = checkBox;
+    itemCheckBox.addEventListener("click", checkBoxHandler);
+    toDoList.appendChild(newItem);
 }
 
 // Get ancestor container
@@ -115,7 +105,8 @@ function addTask(priority, text, checkBox){
 // Get ancestor container
 
 function getAncestorIfItHasClass(elem, className){
-    // recursively check up the family tree for specific className and return that node
+    // Takes a DOM node (typically from event.target) and recursively checks up the
+    // DOM tree til it finds a node with className. It then returns that node.
     if(elem.classList.contains(className)) return elem;
     if(elem.parentElement) return getAncestorIfItHasClass(elem.parentElement, className);
 }
@@ -128,14 +119,14 @@ function getAncestorIfItHasClass(elem, className){
 // GET
 // GET
 
-test("Get blog route, status should be 200, body is an array encoded as json", t => {
+test("Test GET route description", t => {
     supertest(router)
-    .get("/blog")
+    .get("/the-route-name")
     .expect(200)
     .expect("content-type", "application/json")
     .end( (err, res) => {
         t.error(err);
-        t.equal(res.text, JSON.stringify(["cat", "dog", "bird"]));
+        t.equal(res.text, JSON.stringify(["some", "json", "data"]));
         t.end();
     });
 });
@@ -144,16 +135,16 @@ test("Get blog route, status should be 200, body is an array encoded as json", t
 // POST
 // POST
 
-test("Receive blog, status should be 200 and body is ['a', 'b']" , t => {
+test("Test POST route description" , t => {
     supertest(router)
-    .post("/blog")
-    .send(['a', 'b'])
-    .set("Authorization", "123") //sends a request header to the server
+    .post("/the-route-name")
+    .send(['some', 'post', 'data']) // some serializable javascript object
+    .set("some-header", "some value") //sends a request header to the server
     .expect(200)
     .expect("content-type", "application/json")
     .end( (err, res) => {
         t.error(err);
-        t.equal(res.text, JSON.stringify(["a", "b"]));
+        t.equal(res.text, JSON.stringify(['some', 'post', 'data']));
         t.end();
     });
 });
